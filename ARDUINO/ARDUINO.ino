6cfef4d2d146dd5151a2d98631aea34580c7f9d9
INCE <BMEINCE2019@gmail.com>
2019-02-13 14:46:13 -0500

    Moved ARDUINO.ino to correct file

--- ARDUINO/ARDUINO.ino
+++ ARDUINO/ARDUINO.ino
@@ -8,10 +8,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
     int value=analogRead(pin);
-    /*if (value >= 1000){
+    if (value >= 1000){
       value = 999;
-    }*/
-    value = (value*9)/10;
+    }
+    //value = (value*9)/10;
     Serial.print('z');
     Serial.println(value);
     Serial.flush();
